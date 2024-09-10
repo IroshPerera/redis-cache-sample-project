092561196fccc80f54fffa8cb232b2ec9ef82b36
Irosh Perera <irosh@webmotech.com>
2024-09-10 12:07:09 +0530

    few changes

--- src/main/java/com/webmotech/test_redis_with_springboot/TestRedisWithSpringbootApplication.java
+++ src/main/java/com/webmotech/test_redis_with_springboot/TestRedisWithSpringbootApplication.java
@@ -2,8 +2,10 @@ package com.webmotech.test_redis_with_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TestRedisWithSpringbootApplication {
 
 	public static void main(String[] args) {
