532fb8940c3673c8382cd9148eeb920bd27094cd
Irosh Perera <irosh@webmotech.com>
2024-09-10 10:04:27 +0530

    init commit

---
+++ src/main/java/com/webmotech/test_redis_with_springboot/TestRedisWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.webmotech.test_redis_with_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRedisWithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRedisWithSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webmotech/test_redis_with_springboot/TestRedisWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webmotech.test_redis_with_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRedisWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
